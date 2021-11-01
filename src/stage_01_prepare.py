4b5b345742cda0730148835a0f1d747776793fcf
Bijoy99roy <bijoy2014amp@gmail.com>
2021-11-01 13:15:36 +0530

    stage 01 done

--- src/stage_01_prepare.py
+++ src/stage_01_prepare.py
@@ -4,6 +4,7 @@ import shutil
 from tqdm import tqdm
 import logging
 from src.utils.common import read_yaml, create_directory
+from src.utils.data_mgmt import process_posts
 import random
 
 
@@ -41,8 +42,7 @@ def main(config_path, params_path):
     with open(input_data, encoding='utf8') as fd_in:
         with open(train_data_path, 'w', encoding='utf8') as fd_out_train:
             with open(test_data_path, 'w', encoding='utf8') as fd_out_test:
-                pass
-                #process_ports(fd_in, fd_out_train, fd_out_test, '<python>', split)
+                process_posts(fd_in, fd_out_train, fd_out_test, '<python>', split)
 
 if __name__ == '__main__':
     args = argparse.ArgumentParser()
