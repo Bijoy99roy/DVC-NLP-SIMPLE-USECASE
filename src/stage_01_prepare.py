6f4c3f6c8557f4eaacaa950177fd57067df48c7e
Bijoy99roy <bijoy2014amp@gmail.com>
2021-11-01 12:45:33 +0530

    stage 01 tsv empty file created

--- src/stage_01_prepare.py
+++ src/stage_01_prepare.py
@@ -22,7 +22,7 @@ def main(config_path, params_path):
     config = read_yaml(config_path)
     params = read_yaml(params_path)
 
-    source_data = config['source_data']
+    source_data = config['source_download_dirs']
     input_data = os.path.join(source_data['data_dir'], source_data['data_file'])
 
     split = params['prepare']['split']
@@ -35,7 +35,14 @@ def main(config_path, params_path):
 
     create_directory([prepared_data_dir_path]) 
 
+    train_data_path = os.path.join(prepared_data_dir_path, artifacts['TRAIN_DATA'])
+    test_data_path = os.path.join(prepared_data_dir_path, artifacts['TEST_DATA'])
 
+    with open(input_data, encoding='utf8') as fd_in:
+        with open(train_data_path, 'w', encoding='utf8') as fd_out_train:
+            with open(test_data_path, 'w', encoding='utf8') as fd_out_test:
+                pass
+                #process_ports(fd_in, fd_out_train, fd_out_test, '<python>', split)
 
 if __name__ == '__main__':
     args = argparse.ArgumentParser()
