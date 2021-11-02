c10edf4e3211b86e304418afd7df54fcb8cc77c7
Bijoy99roy <bijoy2014amp@gmail.com>
2021-11-02 12:32:02 +0530

    stage 04 train complete

--- src/stage_03_train.py
+++ src/stage_03_train.py
@@ -3,7 +3,13 @@ import os
 import shutil
 from tqdm import tqdm
 import logging
+from src.utils.common import read_yaml, create_directory
+import joblib
+import numpy as np
+from sklearn.ensemble import RandomForestClassifier
 
+
+STAGE = 'THREE'
 
 logging.basicConfig(
     filename=os.path.join("logs", 'running_logs.log'), 
@@ -13,19 +19,53 @@ logging.basicConfig(
     )
 
 def main(config_path, params_path):
-    pass
+    config = read_yaml(config_path)
+    params = read_yaml(params_path)
+    
+    artifacts = config['artifacts']
+
+    featurized_data_dir_path = os.path.join(artifacts['ARTIFACTS_DIR'], artifacts['FEATURIZED_DATA'])
+    featurized_train_data_path = os.path.join(featurized_data_dir_path, artifacts['FEATURIZED_OUT_TRAIN'])
+    
+    model_dir_path = os.path.join(artifacts['ARTIFACTS_DIR'], artifacts['MODEL_DIR'])
+    create_directory([model_dir_path])
+    model_path = os.path.join(model_dir_path, artifacts['MODEL_NAME'])
+
+    matrix = joblib.load(featurized_train_data_path)
+
+    labels = np.squeeze(matrix[:, 1].toarray())
+    x = matrix[:, 2:]
+
+    logging.info(f'Input matrix size: {matrix.shape}')
+    logging.info(f'X matrix size: {x.shape}')
+    logging.info(f'Y  matrix size: {labels.shape}')
+
+    seed = params['train']['seed']
+    n_est = params['train']['n_est']
+    min_split = params['train']['min_split']
+
+    model = RandomForestClassifier(
+        n_estimators=n_est,
+        min_samples_split=min_split,
+        n_jobs=2,
+        random_state=seed
+    )
+
+    model.fit(x, labels)
+
+    joblib.dump(model, model_path)
 
 if __name__ == '__main__':
     args = argparse.ArgumentParser()
-    args.add_argument("--config", "-c", default="config/config.yaml")
+    args.add_argument("--config", "-c", default="configs/config.yaml")
     args.add_argument("--params", "-p", default="params.yaml")
     parsed_args = args.parse_args()
 
     try:
         logging.info("\n********************")
-        logging.info(">>>>> stage one started <<<<<")
+        logging.info(f">>>>> stage {STAGE} started <<<<<")
         main(config_path=parsed_args.config, params_path=parsed_args.params)
-        logging.info(">>>>> stage one completed! all the data are saved in local <<<<<n")
+        logging.info(f">>>>> stage {STAGE} completed! all the data are saved in local <<<<<\n")
     except Exception as e:
         logging.exception(e)
         raise e
