c742205cb0ba55ce5c0a8fd76fbe1812202e0f6e
Bijoy99roy <bijoy2014amp@gmail.com>
2021-11-02 11:08:39 +0530

    example used

---
+++ research/csr_exp.py
@@ -0,0 +1,16 @@
+import numpy as np
+from scipy.sparse import csr_matrix
+
+A = np.array([
+    [1,0,0,1,0,0],
+    [0,0,2,0,0,1],
+    [0,0,0,2,0,0]
+])
+
+print(A)
+
+s = csr_matrix(A)
+print(s)
+B = s.todense()
+
+print(B)
